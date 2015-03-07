40eaafb490aada9b38fe8f710f07dd25d881d1dd
Ethel Ng <ethel@ustwo.com>
2015-03-07 16:21:39 +0000

    Add quick tests for tutorial file attachment

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  before(:each) do
+    @project = Project.new(name: 'Code a blog')
+  end
+
+  it 'has a name' do
+    expect(@project.name).to eq('Code a blog')
+  end
+
+  it 'has a tutorial attachment' do
+    expect(@project.tutorial.path).to eq(nil)
+    File.open('./spec/fixtures/code_a_blog.txt') do |f|
+      @project.tutorial = f
+      @project.save
+    end
+    expect(@project.tutorial.path).not_to eq(nil)
+  end
+end
