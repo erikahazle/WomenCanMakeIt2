a81daca3886539228f31eae5c52205c5a25c2dc8
Ethel Ng <ethel@ustwo.com>
2015-03-08 02:51:46 +0000

    Add seeds and rename fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+blog_project = Project.new(name: 'Build a blog')
+File.open('./spec/fixtures/build_a_blog.txt') do |f|
+  blog_project.tutorial = f
+  blog_project.save
+end
+
+ecommerce_project = Project.new(name: 'Build an e-commerce site')
+File.open('./spec/fixtures/build_an_ecommerce_site.txt') do |f|
+  ecommerce_project.tutorial = f
+  ecommerce_project.save
+end
+
+User.create(first_name: 'Ethel', email: 'ethel@gmail.com', password: 'password', password_confirmation: 'password')
+User.create(first_name: 'Erika', email: 'erika@hotmail.com', password: 'password', password_confirmation: 'password')
