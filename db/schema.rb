43589e6d5126b914a21cae8be82637fc7a22d73b
Ethel Ng <ethel@ustwo.com>
2015-03-08 00:23:36 +0000

    Add active status with default of true to membership model

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308000845) do
+ActiveRecord::Schema.define(version: 20150308001755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "memberships", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
     t.integer  "team_id"
     t.integer  "user_id"
+    t.boolean  "active",     default: true
   end
 
   add_index "memberships", ["team_id"], name: "index_memberships_on_team_id", using: :btree
